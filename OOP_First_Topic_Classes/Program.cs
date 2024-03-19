2ffb004f00f3ea8dcf75f9ddb999d132cbb311f2
Nazar <Nazar@DESKTOP-6OHCM5M>
2024-03-19 18:51:43 +0200

    Добавьте файлы проекта.

---
+++ OOP_First_Topic_Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+class Student {
+
+    public string name { get; set; }
+    public string lastName{get;set;}
+
+   public Student (string name, string lastName)
+    {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+
+    public void print() {
+        Console.WriteLine($"Name: {name}, LastName: {lastName}");
+
+    }
+}
+
+class OOP_First_Topsc_Classes {
+
+
+    public static void Main(string[] args)
+    {
+
+        Student student = new Student("name", "LastName");
+
+        student.print();
+
+
+    
+    
+    }
+
+
+}
